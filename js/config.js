a197f1f10446dbdcff4c71202b76fa7ea2850ead
abdellahsaifeddine <abdellahsaifeddine123@gmail.com>
2026-06-05 16:34:53 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-/**
- * Remplacez whatsappE164 par votre numéro WhatsApp Business (indicatif pays sans +).
- * Exemple Maroc : 2126xxxxxxxx
- */
-window.MIND_CONFIG = {
-  whatsappE164: "212691658691",
-};
